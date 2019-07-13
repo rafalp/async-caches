5f79c7d7f8aa28984f5e7ed31bf2748bc3c43fce
rafalp <kontakt@rpiton.com>
2019-07-13 21:46:39 +0200

    Fix first test

--- tests/test_cache_url.py
+++ tests/test_cache_url.py
@@ -3,4 +3,4 @@ from caches.core import CacheURL
 
 def test_protocol_is_available_as_backend():
     cache = CacheURL("dummy://null")
-    assert cache.dialect == "dummy"
+    assert cache.backend == "dummy"
