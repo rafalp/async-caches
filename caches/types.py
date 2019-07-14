2a17234770d232be030df61406588eed01fa8c53
rafalp <kontakt@rpiton.com>
2019-07-14 02:45:04 +0200

    Format code with black

--- caches/types.py
+++ caches/types.py
@@ -2,7 +2,5 @@ from typing import Any, Collection, Dict, Union
 
 # Note: "Any" should be "Serializable"
 # See https://github.com/python/mypy/issues/7069
-Serializable = Union[
-    bool, float, int, str, Collection[Any], Dict[str, Any]
-]
+Serializable = Union[bool, float, int, str, Collection[Any], Dict[str, Any]]
 Version = Union[int, str]
