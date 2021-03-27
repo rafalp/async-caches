e5bb7a244f5f5bf51e340e08032935d72ffc6494
Vladimír Kašpar <vladimir.kaspar@heureka.cz>
2021-03-27 23:30:46 +0100

    Implementation of PEP 3119 to check if abstract methods are implemented
    - Deleted test case for checking raised `NotImplementedError` because it's not necessary by PEP 3119 ABC checking.

--- caches/backends/base.py
+++ caches/backends/base.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta, abstractmethod
 from typing import Any, Dict, Iterable, Mapping, Optional, Union
 
 import json
@@ -6,54 +7,68 @@ from ..core import CacheURL
 from ..types import Serializable
 
 
-class BaseBackend:
+class BaseBackend(metaclass=ABCMeta):
     def __init__(self, cache_url: Union[CacheURL, str], **options: Any):
         self._cache_url = CacheURL(cache_url)
         self._options = options
 
+    @abstractmethod
     async def connect(self):
         raise NotImplementedError()
 
+    @abstractmethod
     async def disconnect(self):
         raise NotImplementedError()
 
+    @abstractmethod
     async def get(self, key: str, default: Any) -> Any:
         raise NotImplementedError()
 
+    @abstractmethod
     async def set(self, key: str, value: Serializable, *, ttl: Optional[int]) -> Any:
         raise NotImplementedError()
 
+    @abstractmethod
     async def add(self, key: str, value: Serializable, *, ttl: Optional[int]) -> bool:
         raise NotImplementedError()
 
+    @abstractmethod
     async def get_or_set(
         self, key: str, default: Serializable, *, ttl: Optional[int]
     ) -> Any:
         raise NotImplementedError()
 
+    @abstractmethod
     async def get_many(self, keys: Iterable[str]) -> Dict[str, Any]:
         raise NotImplementedError()
 
+    @abstractmethod
     async def set_many(
         self, mapping: Mapping[str, Serializable], *, ttl: Optional[int]
     ):
         raise NotImplementedError()
 
+    @abstractmethod
     async def delete(self, key: str):
         raise NotImplementedError()
 
+    @abstractmethod
     async def delete_many(self, keys: Iterable[str]):
         raise NotImplementedError()
 
+    @abstractmethod
     async def clear(self):
         raise NotImplementedError()
 
+    @abstractmethod
     async def touch(self, key: str, ttl: Optional[int]) -> bool:
         raise NotImplementedError()
 
+    @abstractmethod
     async def incr(self, key: str, delta: Union[float, int]) -> Union[float, int]:
         raise NotImplementedError()
 
+    @abstractmethod
     async def decr(self, key: str, delta: Union[float, int]) -> Union[float, int]:
         raise NotImplementedError()
 
